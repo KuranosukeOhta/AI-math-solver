ea83c9be2400ee97ad5fea71d2df1a7271e1a785
Kuranosuke Ohta <50015337+KuranosukeOhta@users.noreply.github.com>
2025-05-15 14:06:06 +0900

    feat: CORSを許可するミドルウェアを追加

--- static-server.js
+++ static-server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+// CORSを許可
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // 静的ファイルを提供するディレクトリを設定
 app.use('/_next', express.static(path.join(__dirname, '.next')));
 
